456adce2e47fb8923236ba4efd3830f8b534a57e
Mateus Dantas <mateusdantas.dev@gmail.com>
2023-05-08 22:26:17 -0300

    Finalize records

---
+++ src/br/com/javainsider/oo2/records/App.java
@@ -0,0 +1,31 @@
+package br.com.javainsider.oo2.records;
+
+public class App {
+    public static void main(String[] args) {
+
+        Person person = new Person("Mateus", "Dantas", 27);
+        System.out.println(person);
+        System.out.println(person.getFirstName());
+
+        System.out.println("----------NEW PERSON----------");
+        PersonRecord personRecord = new PersonRecord("Mateus", "Dantas", 27);
+        System.out.println(personRecord);
+        System.out.println(personRecord.lastName());
+
+        System.out.println("----------NEW PERSON----------");
+        PersonRecord personRecord2 = new PersonRecord("Mateus", "Dantas", 29);
+        System.out.println(personRecord2.equals(personRecord));
+
+//        PersonRecord personRecord3 = new PersonRecord(null, "Dantas", 29);
+//        System.out.println(personRecord3);
+
+        System.out.println("----------NEW PERSON----------");
+        PersonRecord personRecord4 = new PersonRecord("Mateus", "Nascimento");
+        System.out.println(personRecord4);
+        System.out.println(personRecord4.fullName());
+
+        System.out.println("----------NEW PERSON----------");
+        PersonRecord personRecord5 = PersonRecord.create("Cris");
+        System.out.println(personRecord5);
+    }
+}
