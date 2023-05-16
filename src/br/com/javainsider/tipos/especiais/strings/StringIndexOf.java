34d58999c1b65eb30b4d4801de8cecf955cc879c
Mateus Dantas <mateusdantas.dev@gmail.com>
2023-05-15 22:13:51 -0300

    Finalize Strings

---
+++ src/br/com/javainsider/tipos/especiais/strings/StringIndexOf.java
@@ -0,0 +1,18 @@
+package br.com.javainsider.tipos.especiais.strings;
+
+public class StringIndexOf {
+    public static void main(String[] args) {
+
+        String str = "java is the greatest language!";
+        String str2 = "java is the greatest language! the";
+
+        int pos = str.indexOf("the");
+        System.out.println(pos);
+
+        int pos2 = str2.indexOf("the", 10);
+        System.out.println(pos2);
+
+        int pos3 = str.lastIndexOf("a");
+        System.out.println(pos3);
+    }
+}
---
+++ src/br/com/javainsider/tipos/especiais/strings/StringSubstring.java
@@ -0,0 +1,17 @@
+package br.com.javainsider.tipos.especiais.strings;
+
+public class StringSubstring {
+    public static void main(String[] args) {
+
+        String str = "java is the greatest language!";
+
+        String substr1 = str.substring(4).trim();
+        System.out.println(substr1);
+
+        String substr2 = str.substring(4, str.length() - 1);
+        System.out.println(substr2);
+
+        String substr3 = str.substring(11, 20);
+        System.out.println(substr3);
+    }
+}
