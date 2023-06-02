3ce3947dbacfe88cec54d481fed77900c13c1b62
Mateus Dantas <mateusdantas.dev@gmail.com>
2023-06-02 15:07:26 -0300

    Initialize regular expression module

--- src/br/com/javainsider/java.essencial/textos.numeros/string.numero/ExItems.java
+++ src/br/com/javainsider/java.essencial/textos.numeros/string.numero/ExItems.java
@@ -5,8 +5,8 @@ public class ExItems {
     public static void main(String[] args) {
 
         Item i1 = new Item("Item #1", 550.5, "EUR");
-        Item i2 = new Item("Item #1", 5690, "BRL");
-        Item i3 = new Item("Item #1", 5.55, "USD");
+        Item i2 = new Item("Item #2", 5690, "BRL");
+        Item i3 = new Item("Item #3", 5.55, "USD");
 
         System.out.println(i1);
         System.out.println(i2);
